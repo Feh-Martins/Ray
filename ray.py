25d73f3800132205b01521ab0753309f71dfae33
Feh-Martins <107818781+Feh-Martins@users.noreply.github.com>
2025-07-20 23:28:07 -0300

    Update ray.py

--- ray.py
+++ ray.py
@@ -18,7 +18,7 @@ st.markdown(
     <style>
     .stApp {{
     background: url("data:image/jpeg;base64,{imagem_fundo}");
-    background-size: 48%;
+    background-size: 100%;
     background-repeat: no-repeat;
     background-position: center;
     }}
